09c740633f34e023e8dce4c1493e7e0c426dfa81
rismaduwirahayuni <rismaduwirahayuni@gmail.com>
2022-06-17 09:02:56 +0700

    create seeder

---
+++ db/seeders/20220617013454-categories-seeder.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */ 
+    await queryInterface.bulkInsert('categories', [
+      {
+      name: 'Hoby',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: 'Vehicles',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: 'Clothes',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: 'Electronics',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: 'Health',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
