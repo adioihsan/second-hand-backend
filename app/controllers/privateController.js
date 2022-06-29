253cb52b399db2730a1b32453686cfc97ac41f67
faprayyy <fauzanjr1@gmail.com>
2022-06-29 15:08:36 +0700

    Add Profile API

--- app/controllers/privateController.js
+++ app/controllers/privateController.js
@@ -529,15 +529,21 @@ module.exports = {
     getProfile: async (req, res) => { 
         try {
             const jwtData = req.user;
-            const profile = await user.findOne({ 
+            const profileData = await user.findOne({ 
                 where: { id: jwtData.id },
                 attributes: ['id', 'email'],
                 include: [
                     { model: user_detail, attributes: ['name', 'image'] }
                 ],
+            })
+            if (!profileData) { return response(res, 404, false, 'User not found', null ) }
+
+            return response(res, 200, true, 'Success', {
+                id: profileData.id,
+                email: profileData.email,
+                name: profileData.user_detail.name,
+                photo: profileData.user_detail.image
             });
-            if (!profile) { return response(res, 404, false, 'User not found', profile) }
-            return response(res, 200, true, 'Success', profile);
         } catch (error) {
             console.log(error);
             if (error.name === 'SequelizeDatabaseError') {
@@ -545,6 +551,5 @@ module.exports = {
             }
             return response(res, 500, false, "Internal Server Error", null);
         }
-    }
-    
+    }  
 }
