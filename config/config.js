910d9fd4fce7ed2c9b178225603b470d45e969b9
Alif Raihanza Fauzan <alifraihanzaa@gmail.com>
2022-07-04 06:27:08 -0700

    GET /product/:id/me, UPDATE /product/:id, GET /product/:id

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   development: {
       username: "postgres",  
-      password: "raihanza",  
+      password: "123",  
       database: "db_2ndhand",
       host: "127.0.0.1",
       dialect: "postgres"
