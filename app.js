fdbd450c08eebb1d8718d4232018972a517dca05
adio ihsan <adioihsan@gmail.com>
2024-02-22 21:36:10 +0700

    fix: database config

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var app = express();
-var dotenv = require('dotenv').config()
-const response = require('./utils/formatResponse')
-const indexRouter = require('./routes/index')
+require("dotenv").config();
+const response = require("./utils/formatResponse");
+const indexRouter = require("./routes/index");
 
-const swaggerJSON = require('./swagger.json');
-const swaggerUI = require('swagger-ui-express');
+const swaggerJSON = require("./swagger.json");
+const swaggerUI = require("swagger-ui-express");
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
-
+app.use("/", indexRouter);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
   const status = err.status || 500;
   if (err.name === "NotFoundError") {
-    return response(res, status, false, "Url Not Found", null)
-  } 
-  return response(res, false, status, err.message, null)
+    return response(res, status, false, "Url Not Found", null);
+  }
+  return response(res, false, status, err.message, null);
 });
 
 module.exports = app;
